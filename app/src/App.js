5993a3c2e97f0fd8991d88dae395bb9e81f9a250
Jennifer Kramer <jenkrame@gmail.com>
2021-01-20 18:06:25 -0500

    Added styles and filled out Insults component. WIP

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
+import Insults from './components/Insults';
+
 function App() {
   return (
     <div className="App">
       <h1>Why are you even here?</h1>
       <h2>You must be a masochist if you want to be insulted.</h2>
+      <Insults />
     </div>
   );
 }
---
+++ app/src/components/Insults.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Insults = () => {
+
+    const getInsulted = () => {
+        
+    }
+
+    return(
+        <>
+            <h2>--- Insult here ----</h2>
+            <button onClick={getInsulted}>Get Insulted</button>
+        </>
+    )
+}
+
+export default Insults;
